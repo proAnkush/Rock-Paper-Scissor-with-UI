f92e54f39708088af588645be0b6b81ed322e5a8
Ankush Patil <proankush69@gmail.com>
2021-08-18 15:31:20 +0530

    Fully working

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 // number of times each one won 
 let user_win = 0;
 let com_win = 0;
-let Round = 5;
+let Round = 0;
+let user_input = null;
+let playing = false;
 
 // start
 // play()
@@ -29,71 +31,89 @@ function computerPlay(){
     }
 }
 
-function play(){
-    // Main Game loop
-
-    // total rounds
-
-    while(Round > 0){
-        let user_input = prompt("Rock, Paper or Scissor? ");
-        if(user_input == ""){
-            console.log("No input, Ending the game, Coward!");
-            return;
-        }
-        if(checkValidInput(user_input) == false){
-            console.log("Don't know how to play, Huh! Been there, Done that!");
-            return;
-        }
-        let com_input = computerPlay();
-        printOutcome(user_input, com_input);
-        Round--;
-    }
-    if(user_win > com_win){
-        console.log("Finally, The human is victorious");
-    }else if(user_win < com_win){
-        console.log("I am better than you will ever be.");
+function getInput(e){
+    user_input = e.value;
+    if(Round > 0 && playing==true){
+        start();
     }else{
-        console.log("A tie, what does it even mean! Arrgggggghhhhhh.......");
+        playing = false;
+        document.getElementById("heading2").textContent = "Click the start button to Start the Game!";
+        user_input = null;
     }
-
-    if(prompt("How about another round? yes?").toLowerCase() == "yes"){
-        play();
-    }
+    return;
 }
 
-function checkValidInput(user_input){
-    if(user_input != null || user_input != undefined){
-        let ui = user_input.toLowerCase();
-        if(ui == "rock" || ui == "paper" || ui == "scissor"){
-            return true;
+function play(){
+    // Main Game loop
+    // start button clicked
+
+    //reset
+    user_win = 0;
+    com_win = 0;
+    Round = 5;
+    playing = true;
+    document.getElementById("start").textContent = "Restart";
+    document.getElementById("Yscore").textContent = "Human: " + user_win;
+    document.getElementById("Cscore").textContent = "Computer: " + com_win;
+    document.getElementById("heading2").textContent = "So what will your choice be!";
+    // end reset
+
+    document.getElementById("start").style = "background-color: black";
+    // total rounds
+    //getInput();
+    playing = true;
+
+
+}
+
+function start(){
+    // round start
+    if(Round >= 0){
+        Round--;
+        if(user_input != null){
+            Outcome(user_input, computerPlay() );
         }
-        else{
-            return false;
-        }
-    }else{
-        return false;
+        
+    }
+    if(Round == 0){
+        document.getElementById("start").style = "background-color: red";
+        playing = false;
+        end();
     }
     
 }
+function end(){
+    if(user_win > com_win){
+        document.getElementById("Yscore").textContent = "Human: " + user_win + " The Human is victorious";
+    }else if(user_win < com_win){
+        document.getElementById("Cscore").textContent = "Computer: " + com_win + "          " + " Win is Me!";
+    }else{
+        document.getElementById("Cscore").textContent = "Computer: " + com_win + "          " + " Tie :-(";
+    }
+}
+
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-function printOutcome(user_input, com_input){
+function Outcome(user_input, com_input){
     // for single game
-    if(user_input.toLowerCase() == com_input.toLowerCase()){
-        console.log("There is a Tie...");
+    if((user_input != null) && user_input.toLowerCase() == com_input.toLowerCase()){
+        document.getElementById("heading2").textContent = "Tie";
     }else if(playerWon(user_input, com_input)){
         user_win++;
-        console.log("You win, " + capitalizeFirstLetter(user_input) + " beats " + capitalizeFirstLetter(com_input));
-    }else {
+        document.getElementById("Yscore").textContent = "Human: " + user_win;
+        document.getElementById("heading2").textContent = "Human wins";
+    }else if(!playerWon(user_input, com_input)){
         com_win++;
-        console.log("You lose, " + capitalizeFirstLetter(user_input) + " loses to " + capitalizeFirstLetter(com_input) );
+        document.getElementById("Cscore").textContent = "Computer: " + com_win;
+        document.getElementById("heading2").textContent = "Computer wins";
     }
 }
 
 function playerWon(user_input, com_input){
+
     user_input = user_input.toLowerCase();
     com_input = com_input.toLowerCase();
 
